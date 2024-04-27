f10a4f6945243efc3091861fc93832bf224da38a
CoolShivas <shivas2710cool00@gmail.com>
2024-04-27 09:53:26 +0530

    Use_of_useParams

--- great_quotes/src/App.jsx
+++ great_quotes/src/App.jsx
@@ -1,25 +1,27 @@
 import QuotesDetail from "./pages/QuotesDetail";
 import NewQuotes from "./pages/NewQuotes";
 import AllQuotes from "./pages/AllQuotes";
-import {Route, Switch, Redirect} from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
 import AppName from "./components/AppName";
 
-function App(){
-  return <>
-  <AppName></AppName>
-  <Switch>
-    <Route path="/quotes"> 
-<AllQuotes></AllQuotes>
-    </Route>
-    <Route path="/new-quotes"> 
-<NewQuotes></NewQuotes>
-    </Route>
-    <Route path="/quotes-details"> 
-<QuotesDetail></QuotesDetail>
-    </Route>
-    <Redirect to="/quotes"></Redirect>
-  </Switch>
-  </>
+function App() {
+  return (
+    <>
+      <AppName></AppName>
+      <Switch>
+        <Route path="/quotes">
+          <AllQuotes></AllQuotes>
+        </Route>
+        <Route path="/new-quotes">
+          <NewQuotes></NewQuotes>
+        </Route>
+        <Route path="/quotes-details/:quotesId">
+          <QuotesDetail></QuotesDetail>
+        </Route>
+        <Redirect to="/quotes"></Redirect>
+      </Switch>
+    </>
+  );
 }
 
 export default App;
--- great_quotes/src/pages/QuotesDetail.jsx
+++ great_quotes/src/pages/QuotesDetail.jsx
@@ -1,8 +1,16 @@
+import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 
 
 const QuotesDetail = () => {
+
+   const {quotesId} = useParams();
+   console.log(quotesId);
+
   return (
-    <h2> You are on Quotes Details Page. </h2>
+    <section>
+        <h2> You are on Quotes Details Page. </h2>
+        <p> {quotesId} </p>
+    </section>
   )
 }
 
