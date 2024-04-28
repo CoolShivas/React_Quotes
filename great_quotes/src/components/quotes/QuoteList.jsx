eaaf5a88519660a336610cccc5d10b00ad6531c5
CoolShivas <shivas2710cool00@gmail.com>
2024-04-28 22:41:30 +0530

    use_of_useHistory_and_useLocation_hooks_in_QuoteList_Component

--- great_quotes/src/components/quotes/QuoteList.jsx
+++ great_quotes/src/components/quotes/QuoteList.jsx
@@ -1,17 +1,37 @@
+import { useHistory, useLocation } from "react-router-dom";
 import classes from "./QuoteList.module.css";
 import QuoteItems from "./QuoteItems";
 
+
 const QuoteList = ({dummyData}) => {
+
+  const oldHistory = useHistory();
+  const currLocation = useLocation();
+
+
+
+  const handlerOnSorting = () =>{
+    console.log("location",currLocation);
+    oldHistory.push(`/quotes?arrange=asc`)
+  };
+
   return (
+    <>
+    <div className={classes.quote_list__sorting}>
+      <button onClick={handlerOnSorting}>
+        Sort Ascending 
+      </button>
+    </div>
     <ul className={classes.quote_list__div}>
       {dummyData.map((arr)=>{
-        return <QuoteItems key={arr}
+        return <QuoteItems key={arr.id}
         idABC={arr.id}
         authorABC={arr.author}
         textABC={arr.text}
         ></QuoteItems>
       })}
     </ul> 
+    </>
   )
 }
 
