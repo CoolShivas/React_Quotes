0b06d97a87b522200de341195f18cd1354d52d3b
CoolShivas <shivas2710cool00@gmail.com>
2024-04-29 01:21:08 +0530

    Getting_Creative-with-nested-routes

--- great_quotes/src/pages/QuotesDetail.jsx
+++ great_quotes/src/pages/QuotesDetail.jsx
@@ -1,41 +1,45 @@
+import { Link } from "react-router-dom";
 import NotFound from "./NotFound";
 import classes from "./QuotesDetail.module.css";
 import HighlightedQuote from "../components/quotes/HighlightedQuote";
 import Comment from "../components/Header/Comment";
 import { useParams, Route } from "react-router-dom";
 
-
-
 const DUMMY_QUOTES = [
-  {id:'q1', author:"Max", text:"Learning React is fun!"},
-  {id:'q2', author:"John", text:"Learning React + Vite is fun!"},
+  { id: "q1", author: "Max", text: "Learning React is fun!" },
+  { id: "q2", author: "John", text: "Learning React + Vite is fun!" },
 ];
 
-
 const QuotesDetail = () => {
+  const arr = useParams();
 
-   const arr = useParams();
+  const dataDummy = DUMMY_QUOTES.find((brr) => brr.id === arr.quotesId);
 
-   const dataDummy = DUMMY_QUOTES.find((brr)=>brr.id === arr.quotesId);
-
-   if(!dataDummy)
-   {
-      return (<NotFound/>)
-   }
+  if (!dataDummy) {
+    return <NotFound />;
+  }
 
   return (
     <center>
-        <h2> You are on Quotes Details Page. </h2>
-        {/* <p> {arr.quotesId} </p> */}
-        <HighlightedQuote 
-        textRST={dataDummy.text} 
+      <h2> You are on Quotes Details Page. </h2>
+      {/* <p> {arr.quotesId} </p> */}
+      <HighlightedQuote
+        textRST={dataDummy.text}
         authorRST={dataDummy.author}
-        ></HighlightedQuote>
-        <Route path={`/quotes/${arr.quotesId}/comment`}>
-          <Comment></Comment>
-        </Route>
+      ></HighlightedQuote>
+      <Route exact path={`/quotes/${arr.quotesId}`}>
+        <div className="centered">
+          <Link className="btn__flat" to={`/quotes/${dataDummy}`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+
+      <Route path={`/quotes/${arr.quotesId}/comment`}>
+        <Comment></Comment>
+      </Route>
     </center>
-  )
-}
+  );
+};
 
 export default QuotesDetail;
